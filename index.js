0f1c2af57e6c5399a682f23b8b4b577496b03cf5
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2022-07-27 18:16:47 -0300

    Create Node server.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const { connection } = require("./database/connection");
+const express = require('express');
+const cors = require('cors');
 
-console.log('Hola');
+//Initialize apps
+console.log('Initialized node app');
 
+//Connect to database
 connection();
+
+//Create Node server
+const app = express();
+const port = 3900;
+
+//Configurate cors
+app.use(cors());
+
+//Convert body to js object
+app.use(express.json());
+
+//Create routes
+
+//Create server and listen http requests
+app.listen(port, () => {
+    console.log(`Serve running in port ${port}`);
+});
